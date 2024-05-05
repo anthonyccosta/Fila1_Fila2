9e437fa9fa00aa35689270c1b3933a779667d692
ccosta.anthony@gmail.com <ccosta.anthony@gmail.com>
2024-05-05 15:20:40 -0300

    Adicionar arquivos de projeto.

---
+++ Fila1_Fila2/Numero.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fila1_Fila2
+{
+    internal class Numero
+    {
+        int valor;
+        Numero proximo;
+        public Numero(int numero)
+        {
+            this.valor = numero;
+            this.proximo = null;
+        }
+        public void setProximo(Numero num)
+        {
+            this.proximo = num;
+        }
+        public Numero getProximo()
+        {
+            return this.proximo;
+        }
+        public int getNumero()
+        {
+            return this.valor;
+        }
+        public override string? ToString()
+        {
+            return "" + valor;
+        }
+    }
+}
